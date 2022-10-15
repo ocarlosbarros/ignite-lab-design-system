966a1362aece0d41205819103c370c330cf6d7aa
Carlos Barros <carlos_dbs@hotmail.com>
2022-10-15 15:19:46 -0300

    docs(Text.stories):creates storybook

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,78 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Text, TextProps }  from './Text'
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem ipsum.',
+        size: 'md',
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {
+    argTypes: {
+        asChild:{
+            table: {
+                disable: true
+            }
+        }
+    }
+}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    },
+    argTypes: {
+        asChild:{
+            table: {
+                disable: true
+            }
+        }
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    },
+    argTypes: {
+        asChild:{
+            table: {
+                disable: true
+            }
+        }
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Paragraph</p>
+        ),
+        size: 'lg'
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        },
+        asChild: {
+            table: {
+                disable: true,
+            }
+        }
+    }
+}
